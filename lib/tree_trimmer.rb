59f2e4d1aee080112a347fdc6adee9de2433df2b
David Begin <davidmichaelbe@gmail.com>
2015-08-28 20:05:41 -0700

    grab branches with IO.popen

--- lib/tree_trimmer.rb
+++ lib/tree_trimmer.rb
@@ -1,7 +1,12 @@
 require "tree_trimmer/version"
+require "pry"
 
 module TreeTrimmer
   def self.lets_clean_up_some_branches
-    puts "Time to clean up some git branches"
+    puts "\nBranches to Clean:\n\n"
+    branches = IO.popen("git branch").each_line.map(&:chomp).map(&:lstrip)
+    puts branches
   end
 end
+
+TreeTrimmer.lets_clean_up_some_branches
