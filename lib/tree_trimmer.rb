c8484133425686ec2bf2ce41469f2d3641ecfcb3
David Begin <davidmichaelbe@gmail.com>
2015-08-28 21:30:10 -0700

    selecting between branches

--- lib/tree_trimmer.rb
+++ lib/tree_trimmer.rb
@@ -1,11 +1,29 @@
 require "tree_trimmer/version"
-require "pry"
+require "downup"
 
 module TreeTrimmer
   def self.lets_clean_up_some_branches
     puts "\nBranches to Clean:\n\n"
     branches = IO.popen("git branch").each_line.map(&:chomp).map(&:lstrip)
-    puts branches
+    sanitize_branches!(branches)
+
+    branch_options = ("a".."z").take(branches.count)
+      .zip(branches).each_with_object({}) do |option, hash|
+        hash[option.first] = option.last
+      end
+
+    puts selection = Downup::Base.new(
+      options: branch_options,
+      type: :multi_select
+    ).prompt
+  end
+
+  def self.sanitize_branches!(branches)
+    branches.each do |branch|
+      if branch.include?("master")
+        branches.delete(branch)
+      end
+    end
   end
 end
 
